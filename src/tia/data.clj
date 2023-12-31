c85144a419a2b296970bb8880d40b77af1ad427b
Darren Kim <darren.minsoo.kim@gmail.com>
2023-12-31 16:28:46 -0500

    defined address and club schema

--- src/tia/data.clj
+++ src/tia/data.clj
@@ -11,3 +11,84 @@
 
 (def text-css
   "text/css")
+
+(def continents
+  {:north-america {:full-name "North America"
+                   :countries #{:usa :canada}}
+   :south-america {:full-name "South America"
+                   :countries #{:mexico}}})
+
+(def countries
+  {:usa {:full-name "United States"
+         :states #{:al :ak :az :ar :ca :co :ct :de :dc :fl
+                   :ga :hi :id :il :in :ia :ks :ky :la :me
+                   :md :ma :mi :mn :ms :mo :mt :ne :nv :nh
+                   :nj :nm :ny :nc :nd :oh :ok :or :pa :ri
+                   :sc :sd :tn :tx :ut :vt :va :wa :wv :wi
+                   :wy}}
+   :canada {:full-name "Canada"
+            :states #{:on :qc :ns :mb :bc :pe :sk :ab :nl :nb}}
+   :mexico {:full-name "Mexico"}})
+
+(def states
+  {:al {:full-name "Alabama"}
+   :ak {:full-name "Alaska"}
+   :az {:full-name "Arizona"}
+   :ar {:full-name "Arkansas"}
+   :ca {:full-name "California"}
+   :co {:full-name "Colorado"}
+   :ct {:full-name "Connecticut"}
+   :de {:full-name "Delaware"}
+   :dc {:full-name "District of Columbia"}
+   :fl {:full-name "Florida"}
+   :ga {:full-name "Georgia"}
+   :hi {:full-name "Hawaii"}
+   :id {:full-name "Idaho"}
+   :il {:full-name "Illinois"}
+   :in {:full-name "Indiana"}
+   :ia {:full-name "Iowa"}
+   :ks {:full-name "Kansas"}
+   :ky {:full-name "Kentucky"}
+   :la {:full-name "Louisiana"}
+   :me {:full-name "Maine"}
+   :md {:full-name "Maryland"}
+   :ma {:full-name "Massachusetts"}
+   :mi {:full-name "Michigan"}
+   :mn {:full-name "Minnesota"}
+   :ms {:full-name "Mississippi"}
+   :mo {:full-name "Missouri"}
+   :mt {:full-name "Montana"}
+   :ne {:full-name "Nebraska"}
+   :nv {:full-name "Nevada"}
+   :nh {:full-name "New Hampshire"}
+   :nj {:full-name "New Jersey"}
+   :nm {:full-name "New Mexico"}
+   :ny {:full-name "New York"}
+   :nc {:full-name "North Carolina"}
+   :nd {:full-name "North Dakota"}
+   :oh {:full-name "Ohio"}
+   :ok {:full-name "Oklahoma"}
+   :or {:full-name "Oregon"}
+   :pa {:full-name "Pennsylvania"}
+   :ri {:full-name "Rhode Island"}
+   :sc {:full-name "South Carolina"}
+   :sd {:full-name "South Dakota"}
+   :tn {:full-name "Tennessee"}
+   :tx {:full-name "Texas"}
+   :ut {:full-name "Utah"}
+   :vt {:full-name "Vermont"}
+   :va {:full-name "Virginia"}
+   :wa {:full-name "Washington"}
+   :wv {:full-name "West Virginia"}
+   :wi {:full-name "Wisconsin"}
+   :wy {:full-name "Wyoming"}
+   :on {:full-name "Ontario"}
+   :qc {:full-name "Quebec"}
+   :ns {:full-name "Nova Scotia"}
+   :mb {:full-name "Manitoba"}
+   :bc {:full-name "British Columbia"}
+   :pe {:full-name "Prince Edward Island"}
+   :sk {:full-name "Saskatchewan"}
+   :ab {:full-name "Alberta"}
+   :nl {:full-name "Newfoundland and Labrador"}
+   :nb {:full-name "New Brunswick"}})
--- src/tia/model.clj
+++ src/tia/model.clj
@@ -1,6 +1,7 @@
 (ns tia.model
   (:require
    [malli.core :as m]
+   [tia.data :as d]
    [tia.util :as u]))
 
 (def tick
@@ -13,3 +14,40 @@
    tick
    {:tick/id (u/uuid)
     :tick/timestamp (u/now)}) :=> true)
+
+(def address
+  [:map
+   [:address/id :uuid]
+   [:address/street [:string {:min 1 :max 30}]]
+   [:address/unit {:optional true} [:string {:min 1 :max 12}]]
+   [:address/city [:string {:min 1 :max 30}]]
+   [:address/state (vec (cons :enum (keys d/states)))]
+   [:address/zip [:string {:min 3 :max 10}]]
+   [:address/country (vec (cons :enum (keys d/countries)))]])
+
+(comment
+  (m/validate
+   address
+   {:address/id #uuid "dc936b81-a896-45cf-8f7b-17eae0044565"
+    :address/street "1 Tona Street"
+    :address/unit "#3"
+    :address/city "Paramus"
+    :address/state :nj
+    :address/zip "06789"
+    :address/country :usa}) :=> true)
+
+(def club
+  [:map
+   [:club/id :uuid]
+   [:club/full-name [:string {:min 1 :max 30}]]
+   [:club/status [:enum :open :closed :temp-closed]]
+   [:club/address.id :uuid]])
+
+(comment
+  (m/validate
+   club
+   {:xt/id #uuid "23f58509-1cbe-4f11-a1d8-6a1fde6a85e4"
+    :club/id #uuid "23f58509-1cbe-4f11-a1d8-6a1fde6a85e4"
+    :club/full-name "A Latin Amigos"
+    :club/status :open
+    :club/address.id #uuid "c1cb1901-d48d-46dc-9ea5-2deb66b4da5c"}))

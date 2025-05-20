256ffa9076252d843ead4f1f4e289d48fa577d5f
Darren Kim <darren.minsoo.kim@gmail.com>
2025-05-20 07:06:30 -0400

    redesign cards layout for multiple items

--- src/cljs/arcaneflare/pages/performers.cljs
+++ src/cljs/arcaneflare/pages/performers.cljs
@@ -1,39 +1,42 @@
-(ns arcaneflare.pages.performers
-  (:require
-   [clojure.string :as cstr]))
+(ns arcaneflare.pages.performers)
+
+(defn performer-card [{:keys [id name thumbnail bio]}]
+  [:div.column.is-one-quarter-desktop.is-half-tablet
+   [:div.card
+    [:div.card-image
+     [:figure.image.is-4by3
+      [:img {:src thumbnail :alt name}]]]
+    [:div.card-content
+     [:p.title.is-5 name]
+     [:p.subtitle.is-6 (subs bio 0 (min 100 (count bio)))]]
+    [:div.card-footer
+     [:a.card-footer-item {:href (str "/performers/" id)} "View"]
+     [:a.card-footer-item "❤️"]]]]) ; or add toggle logic
 
 (defn node [{:keys [query-params]}]
   (let [page (get query-params :page 1)
         performers [{:name "Luna Love"
                      :club "Red Velvet"
                      :specialty "Pole Dancing"
-                     :rating 4.5}
+                     :rating 4.5 :bio "Sweet, sassy, and always the life of the party. Let’s have some fun!"
+                     :thumbnail "https://ei.phncdn.com/pics/users/0011/1141/2901/avatar95326255/(m=ewILGCjadOf)(mh=1MY8K_QAYgdTxVBQ)200x200.jpg"}
                     {:name "Cherry Blaze"
                      :club "Neon Nights"
                      :specialty "Freestyle"
-                     :rating 3.8}]]
-
+                     :rating 3.8
+                     :bio "Known for her elegance and bold moves, she brings high energy and a hint of mystery to every performance."
+                     :thumbnail "https://ei.phncdn.com/pics/users/0025/0867/1561/avatar95398165/(m=ewILGCjadOf)(mh=s07sVGxbzRIxIlHV)200x200.jpg"}]]
     [:section.section
      [:div.container
-      [:h1.title "Featured Performers" page]
-
+      [:div.box
+       [:div.field.has-addons
+        [:div.control
+         [:input.input {:type "text" :placeholder "Search performers..."}]]
+        [:div.control
+         [:button.button.is-info "Search"]]]]
       [:div.columns.is-multiline
-       (for [{:keys [name] :as performer} performers]
-         [:div.column.is-full-mobile.is-half-tablet.is-one-third-desktop
-          {:key name}
-          [:div.card
-           [:div.card-image
-            [:figure.image.is-4by3
-             [:img {:src (:image performer) :alt (:name performer)}]]]
-
-           [:div.card-content
-            [:p.title.is-5 (:name performer)]
-            [:p.subtitle.is-6 (:club performer)]
-            [:p [:strong "Specialty: "] (:specialty performer)]
-            [:p [:strong "Rating: "] (str "⭐ " (:rating performer))]]
-
-           [:footer.card-footer
-            [:a.card-footer-item.button.is-link.is-fullwidth
-             {:href (str "/performers/" (cstr/lower-case
-                                         (cstr/replace (:name performer) " " "-")))}
-             "View Profile"]]]])]]]))
+       (for [{:keys [id] :as performer} performers]
+         ^{:key id} [performer-card performer])]
+      [:nav.pagination.is-centered {:role "navigation" :aria-label "pagination"}
+       [:a.pagination-previous "Previous"]
+       [:a.pagination-next "Next"]]]]))
--- src/cljs/arcaneflare/pages/places.cljs
+++ src/cljs/arcaneflare/pages/places.cljs
@@ -1,44 +1,89 @@
 (ns arcaneflare.pages.places
   (:require
-   [arcaneflare.state.loaded :as loaded-state]))
+   [reagent.core :as r]))
+
+(defonce search-text
+  (r/atom nil))
+
+(defonce selected-city
+  (r/atom nil))
+
+(defonce dummy-places
+  [{:handle "a1b2c3-skyroom"
+    :name "Skyroom"
+    :location "Gangnam, Seoul"
+    :rating 4.6
+    :thread-count 12
+    :thumbnail "https://cdn.nysapphire.com/wp-content/uploads/2024/05/29191727/SapphireOnly-1.png"}
+   {:handle "d4e5f6-midnight"
+    :name "Midnight Club"
+    :location "Itaewon, Seoul"
+    :rating 4.2
+    :thread-count 8
+    :thumbnail "https://www.rickschicago.com/images/sites/34/hero.mp4"}
+   {:handle "g7h8i9-moonlight"
+    :name "Moonlight"
+    :location "Busan"
+    :rating 4.8
+    :thread-count 15
+    :thumbnail "https://www.rickschicago.com/images/sites/34/1909/ricks7.JPG"}])
 
 (defn card [place]
-  [:div.column.is-full-mobile.is-half-tablet.is-one-third-desktop
-   {:key (:name place)}
+  [:div.column.is-one-third-tablet.is-one-quarter-desktop
    [:div.card
+    [:div.card-image
+     [:figure.image.is-4by3
+      {:style {:overflow "hidden"}}
+      [:img {:src (:thumbnail place)
+             :alt (:name place)
+             :style {:object-fit "cover"
+                     :width "100%"
+                     :height "100%"}}]]]
     [:div.card-content
-     [:p.title.is-5 (:name place)]
-     [:p.subtitle.is-6 (:desc place)]
-     [:div.tags.mb-2
-      (for [tag (:tags place)]
-        [:span.tag.is-info tag])]
-     [:p.has-text-weight-bold.mt-2
-      (str "⭐ " (:rating place))]]]])
+     [:p.title.is-5
+      [:a {:href (str "/places/" (:handle place))}
+       (:name place)]]
+     [:p.subtitle.is-6 (:location place)]
+     [:p "⭐ "
+      (:rating place) " · "
+      (:thread-count place)
+      " threads"]]]])
 
-(defn node [{:keys [query-params]}]
-  (let [page (get query-params :page 1)
-        places @loaded-state/places]
-    [:section.section
-     [:h1 "page: " page]
-     [:div.container
-      ;; === Filter Bar ===
-      [:div.columns.is-multiline.mb-5
-       [:div.column.is-one-third
-        [:div.select.is-fullwidth
-         [:select
-          [:option "All Cities"]
-          [:option "New York"]
-          [:option "Las Vegas"]]]]
-       [:div.column.is-one-third
-        [:div.control
-         [:input.input {:type "text"
-                        :placeholder "Search tags..."}]]]
-       [:div.column.is-one-third
-        [:div.select.is-fullwidth
-         [:select
-          [:option "Sort by Rating"]
-          [:option "Sort by Name"]
-          [:option "Sort by Distance"]]]]]
-      [:div.columns.is-multiline
-       (for [p places]
-         [card p])]]]))
+(defn node []
+  [:section.section
+   [:div.container
+
+    ;; 🔍 Filter + Search
+    [:div.level.mb-5
+     [:div.level-left
+      [:div.select
+       [:select {:value @selected-city
+                 :on-change #(reset! selected-city (-> % .-target .-value))}
+        [:option {:value ""} "All cities"]
+        [:option {:value "seoul"} "Seoul"]
+        [:option {:value "busan"} "Busan"]]]]
+     [:div.level-right
+      [:div.field.has-addons
+       [:div.control
+        [:input.input {:type "text"
+                       :placeholder "Search places"
+                       :value @search-text
+                       :on-change #(reset! search-text (-> % .-target .-value))}]]
+       [:div.control
+        [:button.button.is-info "Search"]]]]]
+
+    ;; 🧱 Cards
+    [:div.columns.is-multiline
+     (for [place (concat dummy-places dummy-places dummy-places)]
+       ^{:key (:handle place)}
+       [card place])]
+
+    ;; ⏬ Pagination
+    [:nav.pagination.is-centered {:role "navigation" :aria-label "pagination"}
+     [:a.pagination-previous "Previous"]
+     [:a.pagination-next "Next"]
+     [:ul.pagination-list
+      [:li [:a.pagination-link.is-current {:aria-label "Page 1"} "1"]]
+      [:li [:a.pagination-link {:aria-label "Goto page 2"} "2"]]
+      [:li [:span.pagination-ellipsis "…"]]
+      [:li [:a.pagination-link {:aria-label "Goto page 5"} "5"]]]]]])

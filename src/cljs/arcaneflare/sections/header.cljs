22c6fc22a2eb982f94015824a7226fd8aa1b08e6
Darren Kim <darren.minsoo.kim@gmail.com>
2025-06-16 07:54:55 -0400

    apply colors on header links and clean

--- src/cljs/arcaneflare/sections/header.cljs
+++ src/cljs/arcaneflare/sections/header.cljs
@@ -97,10 +97,10 @@
        ^{:key item}
        (let [selected? (cstr/includes? path (name item))]
          [:li [:button {:on-click #(rfe/push-state item)
-                        :class [(when selected? 'underline)
+                        :class [(if selected? 'text-green-400 'text-white)
                                 'block 'py-2 'px-3 'rounded-sm 'text-gray-900
                                 'hover:bg-gray-100 'md:hover:bg-transparent
-                                'md:hover:text-blue-700 'dark:text-white
+                                'md:hover:text-blue-700
                                 'md:dark:hover:text-blue-500 'dark:hover:bg-gray-700
                                 'dark:hover:text-white 'md:dark:hover:bg-transparent
                                 'dark:border-gray-700]}
@@ -112,10 +112,9 @@
      [links]]))
 
 (defn menu-on-desktop []
-  [:div {:class ['hidden 'md:block 'md:static
-                 'items-center 'justify-between
-                 'w-full 'md:flex 'md:w-auto
-                 'md:order-1]}
+  [:div {:class ['hidden 'md:block 'items-center
+                 'justify-between 'w-full 'md:flex
+                 'md:w-auto 'md:order-1]}
    [links]])
 
 (defn get-started []
